e7b95f32c7ca7b83ee7394944cf63a8323caf057
ELIZA <rysaeva96@mail.ru>
2022-05-25 15:09:00 +0500

    Решение 2 задачи

---
+++ Task_2/Program2.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдает количество цифр в числе
+
+Console.Write("Введите натуральное число = ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+Console.Write($"Количество цифр в числе {num} = ");
+while (num > 0)
+{
+    num = num / 10;
+    count++;
+} 
+Console.WriteLine(count);
+
+
+
+
