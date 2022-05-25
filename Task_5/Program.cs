2cc42dbb1f9a4ba6584824299ed366a1d3f24070
ELIZA <rysaeva96@mail.ru>
2022-05-25 16:47:28 +0500

    Решение задачи 3 Greg

---
+++ Task_5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит массив 
+// из 8 элементов, заполненный нулями и единицами 
+// в случайном порядке (решение Григория для любых чисел)
+
+int[] Gen_Array()
+{
+    Console.WriteLine("Введите число элементов массива: ");
+    int N = int.Parse(Console.ReadLine());
+    int[] arr = new int[N];
+    Random rand = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0,2);
+    }
+    return arr;
+}
+int[] newarr = Gen_Array();
+
+var str = string.Join(" ", newarr);
+Console.WriteLine(str);
+
+
