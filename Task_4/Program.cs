b4c690a86bac10cbf2924d7098b037a3d9f3d503
ELIZA <rysaeva96@mail.ru>
2022-05-25 15:59:12 +0500

    Решение 4 задачи

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая выведет произведение чисел от 1 до N
+
+Console.WriteLine("Введите число N = ");
+int num = int.Parse(Console.ReadLine());
+int num1 = 1;
+
+for (int i = 1; i <= num; i++)
+{
+num1 = i * num1;
+}
+Console.WriteLine($"Произведение чисел от 1 до N = {num1}");
