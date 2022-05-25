787ec8c6b775302f8b2d5b25605954d9fdd5b17a
ELIZA <rysaeva96@mail.ru>
2022-05-25 16:37:56 +0500

    Решение 3 задачи

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит массив 
+// из 8 элементов, заполненный нулями и единицами 
+// в случайном порядке
+
+int[] arr = new int[8]; // создаем массив из 8 чисел
+int[] PrintArray(int[] array) // создаем метод PrintArray 
+{
+    int count = array.Length; //указываем длину массива
+    for (int i = 0; i < count; i++) // инициализация счетчика
+    {
+        array[i] = new Random().Next(0, 2); //каждому элементу присваиваем число 0 или 1
+        Console.Write(array[i]);
+
+    }
+    return array;
+}
+
+PrintArray(arr);
